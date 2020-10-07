e0a53f436df193a831b612dd8ed9e2e64c26cea7
dilerous <brad.soper@gmail.com>
2020-10-07 15:23:00 -0700

    Update steps.sh

--- steps.sh
+++ steps.sh
@@ -1,3 +1,6 @@
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
 helm repo add minio https://helm.min.io
 kubectl apply -f minionamespace.yaml
 helm install --namespace minio --generate-name minio/minio --set persistence.storageClass=sc-common
