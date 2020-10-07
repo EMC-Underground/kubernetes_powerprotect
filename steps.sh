2b6402b471ed24abbdf481ae8bf75b6be291a9f0
adminuser <adminuser@k8-master01.demo.local>
2020-10-07 18:18:55 -0400

    added steps file

--- steps.sh
+++ steps.sh
@@ -1,2 +1,3 @@
-Helm repo add minio https://helm.min.io
-Helm install --namespace minio --generate-name --set persistence.storeageCall=sc-common
+helm repo add minio https://helm.min.io
+kubectl apply -f minionamespace.yaml
+helm install --namespace minio --generate-name minio/minio --set persistence.storageClass=sc-common
