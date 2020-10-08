83cbc26d95269b753506beaed997f38be51cdbbc
adminuser <adminuser@k8-master01.demo.local>
2020-10-08 11:58:27 -0400

    added sc yaml files

--- steps.sh
+++ steps.sh
@@ -1,6 +1,9 @@
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
-helm repo add minio https://helm.min.io
-kubectl apply -f minionamespace.yaml
+helm repo add harbor https://helm.goharbor.io
+kubectl apply -f harbornamespace.yaml
+kubectl apply -f sc-common.yaml
+kubectl apply -f mysql-sc.yaml
+helm install my-release harbor/harbor --namespace --set persistence.persistentVolumeClaim.redis.storageClass=demo-sc
 helm install --namespace minio --generate-name minio/minio --set persistence.storageClass=sc-common
