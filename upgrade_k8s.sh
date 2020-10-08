6772849636e7ee313ad2873c795de1c966bab914
Craig Smith <nctiggy@gmail.com>
2020-10-08 14:16:48 -0700

    adding upgrade path

--- upgrade_k8s.sh
+++ upgrade_k8s.sh
@@ -4,32 +4,12 @@ export BRANCH="master"
 #############################################
 # Load in the config file
 #############################################
-source <(curl -fsSL https://raw.githubusercontent.com/EMC-Underground/project_colfax/${BRANCH}/bin/config)
-
-#############################################
-# Load in the generate file
-#############################################
-source <(curl -fsSL https://raw.githubusercontent.com/EMC-Underground/project_colfax/${BRANCH}/bin/generate)
+source bin/config
 
 #############################################
 # Load in the software check functions
 #############################################
-source <(curl -fsSL https://raw.githubusercontent.com/EMC-Underground/project_colfax/${BRANCH}/bin/software_checks)
-
-#############################################
-# Load in the vault related functions
-#############################################
-source <(curl -fsSL https://raw.githubusercontent.com/EMC-Underground/project_colfax/${BRANCH}/bin/vault)
-
-#############################################
-# Load in the concourse related functions
-#############################################
-source <(curl -fsSL https://raw.githubusercontent.com/EMC-Underground/project_colfax/${BRANCH}/bin/concourse)
-
-#############################################
-# Load in the input related functions
-#############################################
-source <(curl -fsSL https://raw.githubusercontent.com/EMC-Underground/project_colfax/${BRANCH}/bin/input)
+source bin/software_checks
 
 pull_repo() {
     local repo_url=$1 repo_name=`echo $1 | awk -F'/' '{print $NF}' | awk -F'.' '{print $1}'`
@@ -52,11 +32,8 @@ generate_repo_url() {
     fi
 }
 
+main() {
+  software_pre_reqs
+}
 
-kubernetes_version=`kubectl version --short | awk 'END{print $3}'`
-apt update
-apt-mark unhold kubeadm
-apt install -y kubeadm=1.15.12-00
-apt-mark hold kubadm
-
-pull_repo `generate_repo_url "github.com" "EMC-Underground" "kubernetes_powerprotect"
+main
