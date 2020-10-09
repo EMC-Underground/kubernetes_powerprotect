11b3aa69b121fdaa8189dcdb34e71f4ebe434ac3
Craig Smith <nctiggy@gmail.com>
2020-10-09 09:54:39 -0700

    adding upgrade path

--- steps.sh
+++ steps.sh
@@ -4,7 +4,7 @@ kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manife
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 hostname=`hostname`
-kubectl apply -f metal-lb-config.yaml
+kubectl apply -f ${hostname}-metallb.yaml
 read -p "Press any key to continue"
 
 echo "deploying contour+envoy"
